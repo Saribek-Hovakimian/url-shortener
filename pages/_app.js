2042a1c8ccc41aa84ff442233e2bedb568dd4676
saribek-hovakimian <h.saribek@gmail.com>
2021-12-14 00:37:48 -0500

    Add basic ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: '#9457eb',
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider> 
+  );
 }
 
 export default MyApp
